1b8e52ee4f2434be44828e359ab0ec83d02bf2f4
sush1 <pavelsirotkin@MacBook-Air.local>
2023-07-31 16:48:42 +0300

    fix: реализовал объекты и тесты к ним

--- server/interfaces/move.py
+++ server/interfaces/move.py
@@ -1,7 +1,43 @@
+from typing import Any
+from common import *
 from abc import ABC, abstractmethod
 
 
 class IMoveable(ABC):
     @abstractmethod
-    def do(self, *args):
+    def get_position(self, *args):
         pass
+
+    @abstractmethod
+    def set_position(self, *args):
+        pass
+    
+    @abstractmethod
+    def get_velocity(self, *args):
+        pass
+
+
+class MoveableAdapter(IMoveable):
+    position_property = 'position'
+    velocity_property = 'velocity'
+    
+    def __init__(self, obj: UObject) -> None:
+        self.obj = obj
+
+    def get_position(self):
+        return self.obj.get_property(MoveableAdapter.position_property)
+
+    def get_velocity(self):
+        # raise NotImplementedError
+        return self.obj.get_property(MoveableAdapter.velocity_property)
+
+    def set_position(self, new_position: Point):
+        return self.obj.set_property(MoveableAdapter.position_property, new_position)
+
+
+class Move:
+    def __init__(self, obj:IMoveable) -> None:
+        self.obj = obj
+        
+    def __call__(self):
+        self.obj.set_position(self.obj.get_position()+self.obj.get_velocity())
---
+++ server/tests/test_common.py
@@ -0,0 +1,16 @@
+from interfaces.common import *
+# TODO: пофиксить импорт
+
+
+def test_point():
+    assert Point(5, 10) == (5, 10)
+
+def test_point_add():
+    a = Point(5, 10)
+    b = Point(2, 1)
+    assert a+b == (7,11)
+
+def test_point_sub():
+    a = Point(5, 10)
+    b = Point(2, 1)
+    assert a-b == (3,9)
